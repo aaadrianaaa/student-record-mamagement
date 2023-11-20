8bf2f3ec9fdd851c78c7d9a416ad03a9425262fb
aaadrianaaa <149747588+aaadrianaaa@users.noreply.github.com>
2023-11-20 15:10:32 +0800

    Add files via upload

---
+++ Yana calculateAverageScores() (1).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <set>
+#include <cctype>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+//Declare Arrays
+const int numOfStudents = 50;
+int studentID[numOfStudents];
+string studentName[numOfStudents];
+double scores[numOfStudents];
+int attendance[numOfStudents];
+
+double calculateAverageScores()
+{
+	//declaring variables
+	double totalScores = 0.0, averageScores;
+	
+	//for loop based on totalStudents
+	for(int i = 0; i < numOfStudents; ++i )
+	{
+		totalScores= scores[i]+totalScores; // an equation to sum up all the scores
+	}
+	
+	averageScores= totalScores/numOfStudents; // divide 
+	
+	return averageScores;
+}
+
+int main()
+{
+	
+	cout<<calculateAverageScores();
+	
+	return 0;
+}
